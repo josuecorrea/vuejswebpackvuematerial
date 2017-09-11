4bd23b66b5683400b65d1ca72310b00de4a8d9d2
josuecorrea <josuejtc@gmail.com>
2017-09-10 21:26:07 -0300

    flags

--- src/main.js
+++ src/main.js
@@ -11,8 +11,18 @@ new Vue({
   data: {
     times: [
       new Time("Corinthians", require('./assets/logo60.png')),
-      new Time("Corinthians", require('./assets/logo60.png')),
-      new Time("Corinthians", require('./assets/logo60.png'))
+      new Time("Atletico Mineiro", require('./assets/AtleticoMineiro.png')),
+      new Time("Atletico Goiano", require('./assets/atleticogoiano.png')),
+      new Time("Atletico Paranaense", require('./assets/atleticoparanaense.png')),
+      new Time("Botafogo", require('./assets/botafog.png')),
+      new Time("Ceara", require('./assets/ceara.png')),
+      new Time("Chapecoense", require('./assets/chapecoense.png')),
+      new Time("Flamengo", require('./assets/flamengo.png')),
+      new Time("Fluminense", require('./assets/fluminense.png')),
+      new Time("Gremio", require('./assets/gremio.png')),
+      new Time("Internacional", require('./assets/internacional.png')),
+      new Time("Palmeiras", require('./assets/palmeiras.png')),
+      new Time("Santos", require('./assets/santos.png'))
     ]
   }
 })
